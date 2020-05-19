6f13801d7c2a17620bea412b504f0c3251bc15b8
Povilas Plauska <plauska.povilas@gmail.com>
2020-05-19 21:33:12 +0300

    1

--- client/src/router/auth-guard.js
+++
@@ -1,9 +0,0 @@
-import store from '../store/index';
-
-export default (to, from, next) => {
-  if (store.getters.token !== null && store.getters.token !== undefined) {
-    next();
-  } else {
-    next('/signin');
-  }
-};
